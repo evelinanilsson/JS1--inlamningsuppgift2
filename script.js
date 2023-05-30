9fd98a44a81652eb0df974a732b2e07cc163fd82
evelinanilsson <evelina.nilsson@mail.com>
2023-05-30 15:53:22 +0200

    added date and time

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let apiKey = "4f8ae2347e767f703527c7565ebb4541";
-let apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=london";
+let apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=lund";
 let weatherArticle = document.querySelector(".weather");
 function getWeather () {
     fetch (apiUrl + `&appid=${apiKey}`) 
@@ -8,15 +8,15 @@ function getWeather () {
         console.log(value);
         
         let weather = document.createElement("h1");
-        weather.innerText = value.weather[0].main + " " + value.weather[0].description;
+        weather.innerText = value.weather[0].description.charAt(0).toUpperCase() + value.weather[0].description.slice(1);
         let city = document.createElement("p");
         city.innerText = value.name;
         let temprature = document.createElement("p");
         temprature.innerText = value.main.temp + "°C";
         let lufttryck = document.createElement("p");
-        lufttryck.innerText = value.main.pressure;
+        lufttryck.innerText = "Lufttryck: " + value.main.pressure + " hPa";
         let humidity = document.createElement("p");
-        humidity.innerText = value.main.humidity;
+        humidity.innerText = "Luftfuktighet: " + value.main.humidity + "%";
         let wind = document.createElement("p");
         wind.innerText = value.wind.speed + "m/s";
 
@@ -25,3 +25,12 @@ function getWeather () {
         }))
 }
 getWeather();
+
+// hämta tid och datum
+
+let dateAndTime = document.getElementById("date-time");
+
+setInterval(() => {
+   let date  = new Date();
+    dateAndTime.innerHTML = date.toString().substring(0,24);
+},1000)
