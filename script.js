29a4a454a8617f68c9d5b94faae1c0c9ebc3aec7
evelinanilsson <evelina.nilsson@mail.com>
2023-06-01 23:21:34 +0200

    cleaned up the script and added buttons for favorites

--- script.js
+++ script.js
@@ -1,30 +1,71 @@
 let apiKey = "4f8ae2347e767f703527c7565ebb4541";
-let apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=lund";
+let apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+
+let searchBox = document.querySelector(".choose-weather-address input");
+let searchBtn = document.querySelector(".choose-weather-address button");
+
 let weatherArticle = document.querySelector(".weather");
+let city = "Lund";
+let weatherData = [];
+let lund = document.getElementById("lund");
+let stockholm = document.getElementById("stockholm");
+let västervik = document.getElementById("västervik");
+
 function getWeather () {
-    fetch (apiUrl + `&appid=${apiKey}`) 
+    
+    fetch (apiUrl + city + `&appid=${apiKey}`) 
         .then ((res) => res.json())
         .then ((value => {
         console.log(value);
-        
-        let weather = document.createElement("h1");
-        weather.innerText = value.weather[0].description.charAt(0).toUpperCase() + value.weather[0].description.slice(1);
-        let city = document.createElement("p");
-        city.innerText = value.name;
-        let temprature = document.createElement("p");
-        temprature.innerText = value.main.temp + "°C";
-        let lufttryck = document.createElement("p");
-        lufttryck.innerText = "Lufttryck: " + value.main.pressure + " hPa";
-        let humidity = document.createElement("p");
-        humidity.innerText = "Luftfuktighet: " + value.main.humidity + "%";
-        let wind = document.createElement("p");
-        wind.innerText = value.wind.speed + "m/s";
 
-        weatherArticle.append(weather, city, temprature, lufttryck, humidity, wind);
+        // weatherData.push(value);
+        // console.log(weatherData);
+
+        weatherData = value;
+        console.log(weatherData);
+        drawWeather(weatherData)
         
         }))
 }
-getWeather();
+
+function drawWeather(weatherData) {
+    document.getElementById("city").innerHTML = weatherData.name;
+    document.getElementById("temp").innerHTML = weatherData.main.temp + "°C";
+    document.getElementById("fuktighet").innerHTML = weatherData.main.humidity + "%";
+    document.getElementById("lufttryck").innerHTML = weatherData.main.pressure + "hPa";
+    document.getElementById("vind").innerHTML = weatherData.wind.speed + "m/s";
+    document.getElementById("description").innerHTML = weatherData.weather[0].main;
+
+    if (weatherData.weather[0].main === "Clouds"){
+        document.getElementById("background-img").style.backgroundImage="url(weather-app-img/images/clouds.webp)";
+    }
+    else if (weatherData.weather[0].main === "Clear"){
+        document.getElementById("background-img").style.backgroundImage="url(weather-app-img/images/balloons.webp)";
+    }
+}
+
+//söka efter ny stad
+searchBtn.addEventListener("click", () => {
+    city = searchBox.value;
+    getWeather();
+})
+
+// favoritstäder snabbval
+
+lund.addEventListener("click", () => {
+    city = "Lund";
+    getWeather();
+})
+
+stockholm.addEventListener("click", () => {
+    city = "Stockholm";
+    getWeather();
+})
+
+västervik.addEventListener("click", () => {
+    city = "Västervik";
+    getWeather();
+})
 
 // hämta tid och datum
 
